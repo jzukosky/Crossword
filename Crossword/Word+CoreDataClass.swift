bc0a90c2775bf77a453e22fd943792abfc0dfcbf
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-05-14 17:38:13 -0500

    Trying to get this working

---
+++ Crossword/Word+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Word+CoreDataClass.swift
+//  Crossword
+//
+//  Created by Brendan Krekeler on 5/14/19.
+//  Copyright © 2019 Zukosky, Jonah. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Word: NSManagedObject {
+
+}
